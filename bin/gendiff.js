3c0947a69a9dba86e9a852f81da431f52d4eb60a
kaamosdao <kaamosdao@gmail.com>
2021-03-30 12:28:29 +0300

    2 step add format option + eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'; 
+import { Command } from 'commander';
+
 const program = new Command();
 
 program
-  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .arguments('<filepath1> <filepath2>')
+  .description('Compares two configuration files and shows a difference.')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 program.parse();
