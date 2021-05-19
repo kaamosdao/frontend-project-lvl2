4d0243db023407cb88c2c764c69a84b81212e077
kaamosdao <kaamosdao@gmail.com>
2021-05-19 11:24:12 +0300

    Revert "update parsers.js"
    This reverts commit 2b7954886f8bca5576936547a3e20d7bdfa99fda.

--- src/parsers.js
+++ src/parsers.js
@@ -9,20 +9,30 @@ const format = {
   yaml: 'yaml',
 };
 
-const parse = (filepath) => {
-  const formatFile = path.extname(filepath).toLowerCase().slice(1);
+const parse = (filepath1, filepath2) => {
+  const formatFile1 = path.extname(filepath1).toLowerCase().slice(1);
+  const formatFile2 = path.extname(filepath2).toLowerCase().slice(1);
 
-  if (_.has(format, formatFile) === false) {
+  if (_.has(format, formatFile1) === false || _.has(format, formatFile2) === false) {
     throw new Error('Incorrect file extension, expected .json/.yml/.yaml');
   }
 
-  const dataFile = fs.readFileSync(filepath, 'utf-8');
+  const dataFile1 = fs.readFileSync(filepath1, 'utf-8');
+  const dataFile2 = fs.readFileSync(filepath2, 'utf-8');
 
-  return formatFile === format.json ? JSON.parse(dataFile) : yaml.load(dataFile);
+  const isFormat1Json = formatFile1 === format.json;
+  const isFormat2Json = formatFile2 === format.json;
+  const isFormat1Yml = formatFile1 === format.yml || formatFile1 === format.yaml;
+  const isFormat2Yml = formatFile2 === format.yml || formatFile2 === format.yaml;
+
+  if (isFormat1Json && isFormat2Json) {
+    return [JSON.parse(dataFile1), JSON.parse(dataFile2)];
+  }
+  if (isFormat1Yml && isFormat2Yml) {
+    return [yaml.load(dataFile1), yaml.load(dataFile2)];
+  }
+  return isFormat1Json
+    ? [JSON.parse(dataFile1), yaml.load(dataFile2)] : [yaml.load(dataFile1), JSON.parse(dataFile2)];
 };
 
-export default (filepath1, filepath2) => {
-  const data1 = parse(filepath1);
-  const data2 = parse(filepath2);
-  return [data1, data2];
-};
+export default parse;
